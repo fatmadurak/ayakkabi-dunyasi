1badb95df5455325e4766ac87212fd1f6ce8d47c
fatmadurak <fatmabadillidurak@gmail.com>
2023-03-07 12:34:29 +0300

    carousaladded

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -5,7 +5,7 @@ import {Box,Grid,Image} from "@chakra-ui/react"
 import { Link } from '@chakra-ui/react'
 import "./index.css"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 function Home() {
 
 
@@ -85,24 +85,70 @@ function Home() {
 
    </Grid>
    
-  <Box>
+  <Box className='carousalBox'>
 
 
+  <Carousel
+          autoPlay="true"
+          emulateTouch="true"
+          infiniteLoop="true"
+          showArrows="true"
+          className='carousal'
+    
+        >
+          <div>
+            <img className='image'
+              src="https://img-ayakkabidunyasi.mncdn.com/Content/Images/Thumbs/0136161_eclipse-taba-deri-kadin-bot-4783p17301100.jpeg"
+              alt="carouselImage"
+            />
+          </div>
 
-  {/* <Carousel>
-                <div>
-                    <img src="https://img-ayakkabidunyasi.mncdn.com/Content/Images/Thumbs/0150016_provoq-mavi-kadin-terlik-4141k30050100.jpeg" />
-                  
-                </div>
-                <div>
-                    <img src="https://img-ayakkabidunyasi.mncdn.com/Content/Images/Thumbs/0131803_buckhead-turkuaz-kadin-terlik-4686415410100.jpeg" />
-                   
-                </div>
-                <div>
-                    <img src="https://img-ayakkabidunyasi.mncdn.com/Content/Images/Thumbs/0150498_provoq-vizon-kadin-terlik-40291260101110.jpeg" />
-                   
-                </div>
-            </Carousel> */}
+          <div>
+            <img 
+              src="https://img-ayakkabidunyasi.mncdn.com/Content/Images/Thumbs/0141443_provoq-kahve-deri-kadin-bot-4498a2213200.jpeg"
+              alt="carouselImage"
+            />
+          </div>
+
+          
+          <div>
+            <img 
+              src="https://img-ayakkabidunyasi.mncdn.com/Content/Images/Thumbs/0126773_adidas-puremotion-siyah-kadin-ayakkabi-gx5637-2008gx5637.jpeg"
+              alt="carouselImage"
+            />
+          </div>
+       
+          <div>
+            <img 
+              src="https://img-ayakkabidunyasi.mncdn.com/Content/Images/Thumbs/0139920_adidas-racer-lego-beyaz-unisex-spor-ayakkabi-gx2240-2008gx2240.jpeg"
+              alt="carouselImage"
+            />
+          </div>
+          <div>
+            <img 
+              src="https://img-ayakkabidunyasi.mncdn.com/Content/Images/Thumbs/0141347_joma-j-sima-2203-multi-cocuk-spor-ayakkabi-jsimaw2203v-2025jsimaw2203v.jpeg"
+              alt="carouselImage"
+            />
+          </div>
+          <div>
+            <img 
+              src="https://img-ayakkabidunyasi.mncdn.com/Content/Images/Thumbs/0141633_adidas-tensaur-sport-2-0-beyaz-unisex-spor-ayakkabi-gw1987-2008gw1987.jpeg"
+              alt="carouselImage"
+            />
+          </div>
+          <div>
+            <img 
+              src="https://img-ayakkabidunyasi.mncdn.com/Content/Images/Thumbs/0141349_joma-j-sima-2203-multi-cocuk-spor-ayakkabi-jsimaw2203v-2025jsimaw2203v.jpeg"
+              alt="carouselImage"
+            />
+          </div>
+          <div>
+            <img 
+              src="https://img-ayakkabidunyasi.mncdn.com/Content/Images/Thumbs/0126773_adidas-puremotion-siyah-kadin-ayakkabi-gx5637-2008gx5637.jpeg"
+              alt="carouselImage"
+            />
+          </div>
+        </Carousel>
 
 
 
