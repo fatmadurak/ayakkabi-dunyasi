92c9fb9d5c884ba7a9bda2c4354332e47ab77183
fatmadurak <fatmabadillidurak@gmail.com>
2023-03-14 19:17:58 +0300

    committed

--- client/src/Pages/Admin/index.js
+++ client/src/Pages/Admin/index.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import {Box,Button,Flex} from "@chakra-ui/react"
+import { BrowserRouter ,Link, Route, Routes} from 'react-router-dom'
+import AdminOrder from "../Admin/AdminOrder"
+import AdminProducts from "../Admin/AdminProducts"
+
 
 function Admin() {
   return (
-    <div>Admin</div>
+
+   <Box>
+    <Flex justifyContent="center" alignItems="center" mt="300" gap="10">
+   <Box>
+     
+     <Link to="/admin/orders"><Button colorScheme="blue">Siparişler</Button></Link>
+   </Box>
+
+   <Box>
+    
+    <Link to="/admin/products"><Button colorScheme="orange">Ürünler</Button></Link>
+
+
+
+
+
+  </Box>
+
+    </Flex>
+
+
+    <BrowserRouter>
+
+      <Routes>
+
+      <Route path='/'exact  element={<AdminOrder/>} />
+      <Route path='/orders' element={<AdminOrder/>} />
+      <Route path='/products' element={<AdminProducts/>} />
+
+     </Routes>
+
+
+    </BrowserRouter>
+
+
+
+
+    </Box>
+ 
+
   )
 }
 
